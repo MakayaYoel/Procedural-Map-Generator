7d997a3c28eb18449ef5816cff12adefaed7f983
Yoël Makaya <161102138+MakayaYoel@users.noreply.github.com>
2024-11-12 23:27:50 -0500

    reminder flaunt superior intelligence.

--- generation/map_utils.py
+++ generation/map_utils.py
@@ -11,6 +11,14 @@ def map_width(map: list) -> int:
     return len(map[0])
 
 def print_map(map: list):
+    """
+        Fun fact - You can do this all in one line if you want to impress your
+        programmer friends:
+
+        print("\n".join("".join(TILE_WALL_CHAR if map[y][x] == TILE_WALL else TILE_FLOOR_CHAR for x in range(map_width(map))) for y in range(map_height(map))))
+
+        writing readable code is for dweebs :)
+    """
     for y in range(map_height(map)):
         row = ""
         for x in range(map_width(map)):
